7fe2d00e474b9ed516de2c4f5b548850dc19a70a
Alvaro <alvaropiorno100@gmail.com>
2021-03-02 01:08:21 -0300

    done all api routes and persists with firebase

--- controller/urlRoutes.go
+++ controller/urlRoutes.go
@@ -1,9 +1,13 @@
 package controller
 
 import (
-	"net/http"
+	"encoding/json"
 	"log"
+	"net/http"
+	"net/url"
+
 	"github.com/apiorno/UrlShortener/data"
+	"github.com/apiorno/UrlShortener/model"
 	"github.com/gorilla/mux"
 )
 
@@ -12,27 +16,108 @@ type URLAssociationsController struct {
 	l *log.Logger
 }
 
-//Startup initializes the controller
+// Startup initializes the controller
 func Startup(l *log.Logger) *mux.Router {
 	mux := mux.NewRouter()
 	controller := &URLAssociationsController{l}
+
 	getRouter := mux.Methods(http.MethodGet).Subrouter()
-	getRouter.HandleFunc("/{id:[0-9a-v]{20}}", controller.redirectIfExist())
-	
+	getRouter.HandleFunc("/{id:[0-9a-v]{20}}", controller.RedirectIfExist())
+	getRouter.HandleFunc("/", controller.GetAllAssociatedURLs())
+
+	postRouter := mux.Methods(http.MethodPost).Subrouter()
+	postRouter.HandleFunc("/", controller.AssociateURL())
+
+	deleteRouter := mux.Methods(http.MethodDelete).Subrouter()
+	deleteRouter.HandleFunc("/{id:[0-9a-v]{20}}", controller.DisassociateURL())
+
 	return mux
 }
 
-func (c *URLAssociationsController) redirectIfExist() http.HandlerFunc {
+// LogLine logs the text using the controller's logger
+func (c *URLAssociationsController) LogLine(text string) {
+	c.l.Println(text)
+}
+
+// GetAllAssociatedURLs return all the URLs associations
+func (c *URLAssociationsController) GetAllAssociatedURLs() http.HandlerFunc {
 	return func(rw http.ResponseWriter, r *http.Request) {
+		c.LogLine("Handle GET")
+
+		urlAssocs, err := data.FindAllURLs()
+		if err != nil {
+			c.LogLine(err.Error())
+			http.Error(rw, "Can not retrieve data from firebase", http.StatusInternalServerError)
+		}
+		err = json.NewEncoder(rw).Encode(urlAssocs)
+
+		if err != nil {
+			http.Error(rw, "Can not convert Url Associations to JSON", http.StatusInternalServerError)
+		}
+	}
+}
+
+// RedirectIfExist redirects the request id to its associated real url if exists
+func (c *URLAssociationsController) RedirectIfExist() http.HandlerFunc {
+	return func(rw http.ResponseWriter, r *http.Request) {
+		c.LogLine("Handle GET/{id} ")
+
 		id := mux.Vars(r)["id"]
 
 		urlAssoc, err := data.FindURL(id)
 
 		if err != nil {
-			http.Error(rw,err.Error(),http.StatusBadRequest)
+			http.Error(rw, err.Error(), http.StatusBadRequest)
 			return
 		}
 
-		http.Redirect(rw,r,urlAssoc.URL,http.StatusMovedPermanently)
+		http.Redirect(rw, r, urlAssoc.URL, http.StatusMovedPermanently)
+	}
+}
+
+//AssociateURL generates a unique id to associate to the URL if it is a valid URL
+func (c *URLAssociationsController) AssociateURL() http.HandlerFunc {
+	return func(rw http.ResponseWriter, r *http.Request) {
+		c.LogLine("Handle POST ")
+
+		urlAssociation := &model.URLAssociation{}
+		err := urlAssociation.FromJSON(r.Body)
+
+		if err != nil {
+			c.LogLine(err.Error())
+			http.Error(rw, "Unable to parse json", http.StatusBadRequest)
+			return
+		}
+
+		_, err = url.ParseRequestURI(urlAssociation.URL)
+		if err != nil {
+			http.Error(rw, "Invalid url format", http.StatusBadRequest)
+			return
+		}
+		urlAssociation, err = data.AssociateURL(urlAssociation.URL)
+		if err != nil {
+			http.Error(rw, "Can not associate url", http.StatusInternalServerError)
+		}
+		err = urlAssociation.ToJSON(rw)
+
+		if err != nil {
+			http.Error(rw, "Can not convert to Url Association to JSON", http.StatusInternalServerError)
+		}
+	}
+}
+
+// DisassociateURL removes the association of the URL to the requested id if exists
+func (c *URLAssociationsController) DisassociateURL() http.HandlerFunc {
+	return func(rw http.ResponseWriter, r *http.Request) {
+		c.LogLine("Handle DELETE/{id} ")
+
+		id := mux.Vars(r)["id"]
+
+		err := data.DisassociateURL(id)
+
+		if err != nil {
+			http.Error(rw, err.Error(), http.StatusBadRequest)
+			return
+		}
 	}
 }
--- model/urlassociation.go
+++ model/urlassociation.go
@@ -7,19 +7,18 @@ import (
 
 // URLAssociation represents the association of a short url to a real url
 type URLAssociation struct {
-	UUID string `json:"id"`
-	URL string `json:"url"`
-	
+	UUID string `json:"uuid" firestore:"uuid"`
+	URL  string `json:"url" firestore:"url"`
 }
 
 // ToJSON generates a json representation of URLAssociation
-func (u*URLAssociation) ToJSON(w io.Writer) error {
+func (u *URLAssociation) ToJSON(w io.Writer) error {
 	e := json.NewEncoder(w)
 	return e.Encode(u)
 }
 
 // FromJSON generates an URLAssociation from a json
-func (u*URLAssociation) FromJSON(r io.Reader) error {
+func (u *URLAssociation) FromJSON(r io.Reader) error {
 	d := json.NewDecoder(r)
 	return d.Decode(u)
 }
